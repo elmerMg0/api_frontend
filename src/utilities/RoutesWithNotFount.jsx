6e921cd661758c193df00652578c554e4477a225
elmerMg0 <201801644@est.umss.edu>
2023-04-17 16:50:12 -0400

    page 404 added

--- src/utilities/RoutesWithNotFount.jsx
+++ src/utilities/RoutesWithNotFount.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
-
+import Error404  from '../pages/Error404'
 const RoutesWithNotFount = ( {children} ) => {
   return (
     <Routes>
         {children}
-        <Route path='*' element={<h5>Not found 404</h5>}/>
+        <Route path='*' element={<Error404/>}/>
     </Routes>
   )
 }
